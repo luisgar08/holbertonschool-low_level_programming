39b38ef2a64b87a1ec128d0b50e922761a285f25
root <5832@holbertonstudents.com>
2022-10-11 06:45:52 -0700

    new

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
   /* your code goes there */
   if (n = 0)
     {
-    echo "is zero"
+    print "is zero"
     }
   if (n < 0)
     {
-    echo "is negative"
+    print "is negative"
     }
   if (n > 0)
     {
-     echo "is positive"
+     print "is positive"
     }
   return (0);
 }
