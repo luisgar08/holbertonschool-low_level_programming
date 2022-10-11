13e33e7a60166f6e44656992ecbdd5b361d28e71
root <5832@holbertonstudents.com>
2022-10-11 09:14:39 -0700

    new

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,8 @@ else
 if (nume > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, nume);
-} else
+}
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nume);   
 }
