f8474b992108a60ea94107a9a40391cc7f3065f7
root <5832@holbertonstudents.com>
2022-10-11 07:10:54 -0700

    new

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,14 @@ int main(void)
   n = rand() - RAND_MAX / 2;
   /* your code goes there */
   if (n == 0) {
-      $printf("is zero,\n");
+      printf("%is zero,\n"n);
     }
   if (n < 0) {
-      $printf("is negative\n");
+      printf("%is negative\n"n);
     }
-  if (n > 0) { 
-      $printf("is positive\n");
-    }
+  if (n > 0){
+    printf("%is positive\n"n);
+  }
   return (0);
   
 }
