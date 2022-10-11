845e9b412c5833a847248a9d3ddb48e1f3883d56
root <5832@holbertonstudents.com>
2022-10-11 09:07:18 -0700

    new

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ else
 {
 if (nume > 5)
 {
-printf("Last digit of %i is% i and is greater than 5\n", n, nume);
+printf("Last digit of %d is %d and is greater than 5\n", n, nume);
 }else
 {
-printf("Last digit of %i is% i and is less than 6 and not 0", n, nume);   
+printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, nume);   
 }
 }
 return (0);
