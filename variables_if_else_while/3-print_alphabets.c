60e249befc1aaf929c3b057dd6ded976a09c2450
root <5832@holbertonstudents.com>
2022-10-11 11:13:08 -0700

    new

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 /**
-*
-*
-/*
+ *main - function main
+ *Description: print alphabet in lower
+ *Return: zero
+ */
 int main(void)
 {
+  char ch;
+  for (ch = 'a' ; ch <='z' ; ch++)
+    {
+      printf("%c", ch);
+    }
+  for (ch='A'; ch <= 'z'; ch++)
+    {
+      printf("%c\n", ch);
+    }
   return (0);
 }
