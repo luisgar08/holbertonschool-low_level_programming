d9f1cf7a79ebb8a0f38edb092df4900cd2d5e82c
root <5832@holbertonstudents.com>
2022-10-11 12:09:56 -0700

    new

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *main - function main
+ *Description: print number
+ *Return: zero
+ */
+int main(void)
+{
+  int n;
+  for (n = 0; n <= 9; n++)
+    {
+      putchar("%d", n);
+    }
+  putchar('\n');
+  return (0);
+}
