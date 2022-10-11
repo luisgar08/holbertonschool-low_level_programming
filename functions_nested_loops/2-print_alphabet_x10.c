1ae6df9d810cf5f6056843b649651bc8c71fafa1
root <5832@holbertonstudents.com>
2022-10-11 14:23:34 -0700

    new

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-  int i;
-  char ch;
-  for (i = '0'; i < 10; i++)
-    {
-  for (ch = 'a'; ch <= 'z'; ch++)
-    {
-      _putchar(ch);
-    }
-  _putchar('\n');
-    }
+int i;
+char ch;
+for (i = '0'; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
 }
