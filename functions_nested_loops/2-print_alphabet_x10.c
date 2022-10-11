a7bfe89c39d8f52131dc53157496cfad0257ccb0
root <5832@holbertonstudents.com>
2022-10-11 14:14:36 -0700

    new

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+  int i;
+  char ch;
+  for (i = '0'; i <= 10; ch++)
+    {
+  for (ch = 'a'; ch <= 'z'; ch++)
+    {
+      _putchar(ch);
+    }
+  _putchar('\n');
+    }
+}
