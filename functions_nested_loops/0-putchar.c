561a650580f902164a1351dab454d2997d0c95f2
root <5832@holbertonstudents.com>
2022-10-11 13:39:27 -0700

    new

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *main - function main
+ *Description: print alphabet in lower
+ *Return: zero
+ */
+int main(void)
+{
+  char ch;
+  putchar("_putchar");
+  return (0);
+}
