30f9fbfa8839a4c41fbd3b50471e38f29b4a8418
root <5832@holbertonstudents.com>
2022-10-11 11:42:08 -0700

    new

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-  char ch;
-  char ch2;
-  char ch3;
-    for (ch = 'a'; ch <= 'd'; ch++)
-    {
-      printf("%c", ch);
-    }
-  for (ch2 = 'f'; ch2 <= 'p'; ch2++)
-    {
-      printf("%c", ch2);
-    }
-  for (ch3 = 'r'; ch3 <= 'z'; ch3++)
-    {
-      printf("%c", ch3);
-    }
-  putchar('\n');
-  return (0);
+char ch;
+char ch2;
+char ch3;
+for (ch = 'a'; ch <= 'd'; ch++)
+{
+printf("%c", ch);
+}
+for (ch2 = 'f'; ch2 <= 'p'; ch2++)
+{
+printf("%c", ch2);
+}
+for (ch3 = 'r'; ch3 <= 'z'; ch3++)
+{
+printf("%c", ch3);
+}
+putchar('\n');
+return (0);
 }
