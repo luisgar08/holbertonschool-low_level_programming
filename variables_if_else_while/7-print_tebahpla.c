2dac3e300a56670ae1d7966985d2c9ce60dfd04a
root <5832@holbertonstudents.com>
2022-10-11 12:21:42 -0700

    new

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *main - function main
+ *Description: print alphabet reverse
+ *Return: zero
+ */
+int main(void)
+{
+  char ch;
+  for (ch = 'z' ; ch <='a' ; ch--)
+    {
+      printf("%c", ch);
+    }
+  return (0);
