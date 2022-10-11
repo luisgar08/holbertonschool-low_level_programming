7629163ec0a896e07851a9f71787097344657180
root <5832@holbertonstudents.com>
2022-10-11 11:19:43 -0700

    new

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch <='z' ; ch++)
+for (ch = 'a' ; ch < ='z' ; ch++)
 {
 printf("%c", ch);
 }
-for (ch='A'; ch <= 'Z'; ch++)
+for (ch='A'; ch < = 'Z'; ch++)
 {
 printf("%c", ch);
 }
