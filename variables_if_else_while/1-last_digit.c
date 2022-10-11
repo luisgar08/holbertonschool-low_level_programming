33f0d5d08d398380110d7d9709897afffe585c04
root <5832@holbertonstudents.com>
2022-10-11 07:38:23 -0700

    new

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,16 +12,16 @@ n = rand() - RAND_MAX / 2;
 nume = n % 10;
 if (nume == 0)
 {
-printf("Last digit of %i is% i and is 0\n", n , num);
+printf("Last digit of %i is% i and is 0\n", n , nume);
 }
 else
 {
 if (nume > 5)
 {
-printf("Last digit of %i is% i and is greater than 5\n", n, num);
+printf("Last digit of %i is% i and is greater than 5\n", n, nume);
 }else
 {
-print("Last digit of %i is% i and is less than 5\n", n, num);   
+print("Last digit of %i is% i and is less than 5\n", n, nume);   
 }
 }
 return (0);
