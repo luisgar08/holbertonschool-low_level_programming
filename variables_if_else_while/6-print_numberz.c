b0bf8ebb162e94251e56664012820db6b83ab8e3
root <5832@holbertonstudents.com>
2022-10-11 12:17:53 -0700

    new

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-  int n = '0';
-  while (n <= '9')
-    {
-      putchar(n);
-      n++
-    }
-  putchar('\n');
-  return (0);
+int n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
 }
