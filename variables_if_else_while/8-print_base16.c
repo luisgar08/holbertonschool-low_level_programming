acb2dac5333b9817e505b39535889ca8c64b6966
root <5832@holbertonstudents.com>
2022-10-11 12:35:06 -0700

    new

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - this is function main
+ *Description: reverse alphabet
+ *Return: zero
+ */
+int main(void)
+{
+  char ch = 'z';
+  int n = '0'
+    while (n <= '9')
+      {
+       putchar(n);
+       n++;
+      }
+  while (ch <= 'e')
+    {
+      putchar(ch);
+      ch++;
+    }
+  putchar('\n');
+  return (0);
+}
