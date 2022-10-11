be58f052ef4b45dc4df16f248eab1c7261f4abbb
root <5832@holbertonstudents.com>
2022-10-11 06:50:24 -0700

    new

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int main(void)
   srand(time(0));
   n = rand() - RAND_MAX / 2;
   /* your code goes there */
-  if (n = 0)
+  if ((n == 0))
     {
       print "is zero";
     }
-  if (n < 0)
+  if ((n < 0))
     {
       print "is negative";
     }
-  if (n > 0)
+  if ((n > 0))
     {
       print "is positive";
     }
