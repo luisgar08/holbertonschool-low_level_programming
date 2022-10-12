ef7e7a4a110052c5029efb7acaa422fa7ce8b418
root <5832@holbertonstudents.com>
2022-10-12 13:22:26 -0700

    new

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,16 @@
  */
 int print_last_digit(int n)
 {
-  int x = 0;
-  x = n % 10;
-  if (x < 0)
-    {
-      x = x * -1;
-      _putchar(x + '0');
-    }
-  else
-    {
-      _putchar(x + '0');
-    }
-  return (x);
+int x = 0;
+x = n % 10;
+if (x < 0)
+{
+x = x * -1;
+_putchar(x + '0');
+}
+else
+{
+_putchar(x + '0');
+}
+return (x);
 }
