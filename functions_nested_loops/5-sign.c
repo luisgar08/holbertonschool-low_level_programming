79cb6223c8bd9ff63ae1f1011ceb584d4ebe90b9
root <5832@holbertonstudents.com>
2022-10-12 10:55:00 -0700

    new

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *_isalpha - print if the variable c is lower or upper
+ *@c: variable
+ *Description: print if the variable c is lower or upper
+ *Return: 1 or 0
+ */
+int print_sign(int n)
+{
+  if (c >< 0 )
+    {
+      if ( c >=0 )
+	{
+	  _putchar('+');
+	  return (1)
+	}
+      else
+	{
+	  _putchar('-');
+	  return (-1);
+	}
+    }
+  else
+    {
+      _putchar(0);
+      return (-1);
+    }
+}
