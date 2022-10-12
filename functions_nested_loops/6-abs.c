21f27c390573a117bdf83c2aaa9c608851fbffe4
root <5832@holbertonstudents.com>
2022-10-12 12:26:58 -0700

    new

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -7,8 +7,12 @@
  *Description: print if the variable c is lower or upper
  *Return: 1 or 0
  */
-int r;
-int _abs(int)
+int _abs(int n)
 {
+  if (n < '0')
+    {
+      n=n*-1;
+      printf("%d", n)
+    }
   return (0);
 }
