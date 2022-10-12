1a75903dcc4059fe6748d3d6940067198aa3c70c
root <5832@holbertonstudents.com>
2022-10-12 13:54:04 -0700

    new

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *print_last_digit - print the absolute value of n
+ *@n: variable
+ *Return: n
+ */
+void jack_bauer(void)
+{
+  int h;
+  int m;
+  for (h = 0; h <= 23; h++)
+    {
+      for (m = 0; m <= 59; m++)
+	{
+	  _putchar(h);
+	  _putchar(":");
+	  _puthcar(m);
+	}
+    }
+      return (0);
+}
