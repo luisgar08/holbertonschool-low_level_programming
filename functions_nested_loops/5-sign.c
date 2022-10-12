5ad2c7fef3621100d770a93cf6af60ea92d5fb69
root <5832@holbertonstudents.com>
2022-10-12 12:04:28 -0700

    new

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,23 +2,27 @@
 #include <ctype.h>
 #include "main.h"
 /**
+ *sign - print n how to + or - or 0
+ *@n: variable
+ *Description: print if the variable c is lower or upper
+ *Return: 1, 0 or -1
  */
 
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-    }
-  else if (n < 0)
-    {
-      _putchar('-');
-      return (-1);
-    }
-  else
-    {
-      _putchar('0');
-      return (0);
-    }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
 }
