1f5d4ce2d1511dc983cea5d262dc5a13417bea5d
root <5832@holbertonstudents.com>
2022-10-12 11:31:59 -0700

    new

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,21 +8,21 @@
  *Return: 1, 0 or -1
 */
 
-int print_sign (int n)
+int print_sign(int n)
 {
-  if (n < 0)
-    {
-      _putchar('-');
-      return (-1);
-    }
-  if (n > 0)
-    {
-       _putchar('+');
-      return (1);
-    }
-  if (n == 0)
-    {
-      _putchar('0');
-      return (0);
-    }
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
 }
