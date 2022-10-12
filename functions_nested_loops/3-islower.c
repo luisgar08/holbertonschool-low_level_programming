4403cd5a39f46219ed5e2dc59ec6db164895d5c6
root <5832@holbertonstudents.com>
2022-10-12 10:12:51 -0700

    new

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,22 +1,6 @@
-#include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-/**
- *islower - print if c is lower
- */
-void print_alphabet_x10(void)
-{
-  int i;
-  char ch;
-  for (i = 0; i < 10; i++)
-    {
-      for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	  _putchar(ch);
-	}
-      _putchar('\n');
-    }
-}#include "main.h"
+#include "main.h"
  /**
   *print_alphabet_x10 - print 10 time the alphabet
   */
