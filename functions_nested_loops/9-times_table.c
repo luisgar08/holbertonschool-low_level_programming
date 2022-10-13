e0be17dac851f0aa2cbc9e15821ab696451d289a
root <5832@holbertonstudents.com>
2022-10-12 18:13:03 -0700

    new

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *jack_bauer - print the time to 00:00
+ */
+void jack_bauer(void)
+{
+  int f;
+  int c;
+  for (h = 0; h <= 9; f++)
+    {
+      for (c = 0; c <= 59; c++)
+	{
+	  _putchar((o / 10)',' + '0');
+	  _putchar((0 % 10) + '0');
+	}
+    }
+}
