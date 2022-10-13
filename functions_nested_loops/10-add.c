0ccee2ef0df123574bbaeeada321db1903a00e5b
root <5832@holbertonstudents.com>
2022-10-13 07:37:27 -0700

    new

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *add - print if the variable is lower
+ *@c: variable
+ *Description: print if the variable c is lower
+ *Return: 1 or 0
+ */
+int add(int x, in y)
+{
+  int o;
+  o = x + y;
+  return (o);
+}
