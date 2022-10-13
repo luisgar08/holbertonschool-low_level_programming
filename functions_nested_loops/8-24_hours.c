4e91e0352cf4d1d3bd878846ab4a9fb9bbb379bc
root <5832@holbertonstudents.com>
2022-10-12 17:54:14 -0700

    new

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,20 +10,12 @@ void jack_bauer(void)
 {
   int h;
   int m;
-  int fh;
   for (h = 0; h <= 23; h++)
     {
       for (m = 0; m <= 59; m++)
 	{
-	  while(h >= 10)
-	    {
-	      fh = h;
-	      fh /=10;
-     	    }
-	  _putchar(fh);
-	  _putchar(h % 10);
-	  _putchar(':');
-	  
+	  h / 10;
+	  _putchar(h);
 	}
     }
 }
