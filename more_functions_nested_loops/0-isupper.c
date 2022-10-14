881eeab68d75f90b717d23a26266fdf9b299175c
root <5832@holbertonstudents.com>
2022-10-14 15:41:35 -0700

    new

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ *_isupper - return 1 if c is upper
+ */
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
