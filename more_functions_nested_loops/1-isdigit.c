a76b614b57fc76c6f0413e6203381fdd59008991
root <5832@holbertonstudents.com>
2022-10-14 16:00:00 -0700

    new

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _isdigit -
+ * _isdigit - return to 1 if c is a number
  *@c - variable
- *
+ *Description: return to 1 if c is a number
  */
 int _isdigit(int c)
 {
-  if (c >= '0' && c <= '9')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
