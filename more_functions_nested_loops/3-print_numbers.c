43d632b8ee2953caa8e4ee23f0a74cbb1a1c6e26
root <5832@holbertonstudents.com>
2022-10-14 16:38:39 -0700

    new

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
+ *print_numbers - function
  */
 void print_numbers(void)
 {
-  int n;
-  for (n = '0'; n <= '9'; n++)
-    {
-      _putchar(n);
-    }
-  _putchar('\n');
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
 }
