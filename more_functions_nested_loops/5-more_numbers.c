49e5991bd38f53241d3ab22d7719473511418ff5
root <5832@holbertonstudents.com>
2022-10-14 17:13:48 -0700

    new

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,9 @@ void more_numbers(void)
 {
   int c;
   int n;
-  for (c = '0'; c <= 10; c++)
+  for (c = 0; c <= 10; c++)
     {
-      for (n = '0'; n <= 14; n++)
+      for (n = 0; n <= 14; n++)
 	{
 	  if (n > '9')
 	    {
@@ -20,6 +20,6 @@ void more_numbers(void)
 	      _putchar(n);
 	    }
 	}
-	     printf ("\n");
+      printf ("\n");
     }
 }
