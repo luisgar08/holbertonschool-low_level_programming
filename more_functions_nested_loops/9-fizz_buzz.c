5d9a38730deb86bf00ca6c33a7250118b8891894
root <5832@holbertonstudents.com>
2022-10-14 19:16:04 -0700

    new

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ */
+int main(void)
+{
+  int n;
+  for (n = 0; n < 100; n++)
+    {
+      if (n % 3 == 0 && n % 5 == 0)
+	{
+	  printf("FizzBuzz");
+	}
+      else if (n % 3 == 0)
+	{
+	  printf("Fizz");
+	}
+      else if (n % 5 ==0)
+	{
+	  printf("Buzz");
+	}
+      else
+	{
+	  _putchar(n);
+	}
+      if (n != 100);
+      {
+	printf(" ");
+      }
+      else
+	{
+	  printf("\n");
+	}
+    }
+}
