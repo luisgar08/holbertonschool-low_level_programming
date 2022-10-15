f1bf89bb70172aedbad7195dd28f7a53df00aa59
root <5832@holbertonstudents.com>
2022-10-14 18:19:36 -0700

    new

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ */
+void print_square(int size);
+{
+  int f;
+  int c;
+  if (size > 0)
+    {
+      for (f = 0; f > size; f++)
+	{
+	  for (c = 0; c >size; c++)
+	     {
+	       _putchar('#');
+	     }
+	  _putchar('\n');
+	}
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
