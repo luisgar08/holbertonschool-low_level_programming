36567942b0644f6e40f2ace9d8e0fba3ea2b17c3
root <5832@holbertonstudents.com>
2022-10-14 17:06:54 -0700

    new

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ */
+void more_numbers(void)
+{
+  int c;
+  int n;
+  for (c = '0'; c <= "10"; c++)
+    {
+      for (n = '0'; n <= "14"; n++
+	{
+	  if (n > '9')
+	    {
+	      _putchar((n / 10) + '0');
+	      _putchar((n % 10) + '0');
+	    }
+	  else
+	    {
+	      _putchar(n);
+	    }
+	}
+	     _putchar('\n');
+    }
+}
