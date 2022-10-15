aedd57774b4ddf7482bacc659aed08bb4002e955
root <5832@holbertonstudents.com>
2022-10-14 17:35:27 -0700

    new

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,18 @@
 void print_line(int n)
 {
   int c;
-  for (c = '0'; c <= n; c++)
+  
+  if (n > 0)
     {
-      if (c > 0)
+      for (c = 0; c < n; c++)
 	{
-	  _putchar('_'); 
+	  _putchar('_');
 	}
       _putchar('\n');
     }
-  
+  else
+    {
+      _putchar('\n');
+    }
   c = 0;
 }
