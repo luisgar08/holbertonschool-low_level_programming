5dbdded86dd258bbc15fcecb0b169f41a6665763
root <5832@holbertonstudents.com>
2022-10-14 17:57:31 -0700

    new

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,15 @@ void print_diagonal(int n)
     {
       for (s = 0; s <= n; s++)
 	{
-      for (c = 0; c < n; c++)
-	{
-	  if (n != 1)
+          for (c = 0; c < n; c++)
 	    {
-	      _putchar(' ');
-	    }
-	}
-      _putchar('/');
-      _putchar('\n');
+	       if (s != 1)
+	         {
+	            _putchar(' ');
+	         }
+	     }
+          _putchar("\\");
+          _putchar('\n');
 	}
     }
   else
