d0a26722cca897e07491645ac470079cab42edf7
root <5832@holbertonstudents.com>
2022-10-14 18:37:15 -0700

    new

---
+++ makefiles/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <studio.h>
+/**
+ */
+void print_square(int size)
+{
+  int f;
+  int c;
+  if (size > 0)
+    {
+      for (f = 1; f <= size; f++)
+	{
+	  for (c = 1; c < size; c++)
+	    {
+	      _putchar('#');
+	    }
+	  _putchar('\n');
+	}
+    }
+  else
+    {
+      _putchar('\n');
+    }
+  c = 0;
+}
