27318afec652957665ee2710a46daabf609b2aea
root <5832@holbertonstudents.com>
2022-10-14 19:34:25 -0700

    new

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ */
+void print_triangle(int size)
+{
+  int f;
+  int c;
+  int b;
+  if (size > 0)
+    {
+      for (f = 0; f < size; f++)
+	{
+	  if (b >=j)
+	    {
+	      _putchar('#');
+	    }
+	  else
+	    {
+	      _putchar(' ');
+	    }
+	}
+      b++;
+      _putchar('\n');
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
